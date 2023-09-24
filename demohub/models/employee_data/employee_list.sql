6dea3a185bcf5636ee050cce81130fceb2730790
rtewelde12 <rtewelde12@users.noreply.github.com>
2023-09-24 19:05:24 +0100

    Amended employee list modal

--- demohub/models/employee_data/employee_list.sql
+++ demohub/models/employee_data/employee_list.sql
@@ -6,19 +6,19 @@
 
 with final as(
     select 
-        Employee_ID
-        Full_Name
-        Job_Title
-        Department
-        Business_Unit
-        Gender
-        Ethnicity
-        Age
-        Hire_Date
-        Annual_Salary
-        Bonus_percentages
-        Country
-        City
+        Employee_ID,
+        Full_Name,
+        Job_Title,
+        Department,
+        Business_Unit,
+        Gender,
+        Ethnicity,
+        Age,
+        Hire_Date,
+        Annual_Salary,
+        Bonus_percentages,
+        Country,
+        City,
         Exit_Date
     
     from {{ ref('employee_seed') }}
