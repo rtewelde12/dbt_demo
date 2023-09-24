fbc274b86b0992f95d6df85b0d0ca818237746fd
rtewelde12 <rtewelde12@users.noreply.github.com>
2023-09-24 19:03:19 +0100

    Created a seed and employee list  modal

---
+++ demohub/models/employee_data/employee_list.sql
@@ -0,0 +1,27 @@
+{{
+  config(
+    materialized = "table"
+  )
+}}
+
+with final as(
+    select 
+        Employee_ID
+        Full_Name
+        Job_Title
+        Department
+        Business_Unit
+        Gender
+        Ethnicity
+        Age
+        Hire_Date
+        Annual_Salary
+        Bonus_percentages
+        Country
+        City
+        Exit_Date
+    
+    from {{ ref('employee_seed') }}
+)
+
+select * from final
